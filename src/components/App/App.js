2049904f18ad0c8a0f3b7d732d783b46136c275c
Luis Saturno <saturno.luis@gmail.com>
2020-10-26 23:28:16 -0300

    Add react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import Header from '../Header';
+import SearchResults from '../SearchResults';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 export default function App() {
   return (
-    <main className="App">
-      <Header />
-    </main>
+    <Router>
+      <main className="App">
+        <Header />
+        <Switch>
+            <Route exact path="/">
+              <span>Inicio</span>
+            </Route>
+            <Route path="/items">
+              <SearchResults />
+            </Route>
+          </Switch>
+      </main>
+    </Router>
   );
 }
