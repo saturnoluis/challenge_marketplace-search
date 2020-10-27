c84cf79a8a625f6b5fe713a43d985acc9eefdd3d
Luis Saturno <saturno.luis@gmail.com>
2020-10-26 23:17:23 -0300

    Add SearchResults component

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import './SearchResults.css';
+
+export default function SearchResults() {
+    return(
+      <div className="SearchResults">
+        <section>Breadcrumb...</section>
+        <section>
+          Articles...
+        </section>
+      </div>
+    );
+}
