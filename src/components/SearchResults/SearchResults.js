b09bbcecc05009add5172e8dd03c94af63cb715f
Luis Saturno <saturno.luis@gmail.com>
2020-10-27 22:45:34 -0300

    Add searchResults state

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Items from '../Items';
 import './SearchResults.css';
@@ -5,10 +6,23 @@ import './SearchResults.css';
 export default function SearchResults() {
   const { search: searchParam } = useLocation();
   const search = new URLSearchParams(searchParam).get('search');
+  const [ searchResults, setSearchResults ] = useState(null);
+
+  console.log('Im using search!', search);
+
+  setTimeout(() => {
+    setSearchResults({});
+  }, 5000);
   
+  if(!searchResults) {
+    return(
+      <div>Loading...</div>
+    );
+  }
+
   return(
     <div className="SearchResults">
-      <section>Breadcrumb: {search}</section>
+      <section>Breadcrumb</section>
       <Items />
     </div>
   );
