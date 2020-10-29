eb73585224d3e33b33610a20d7f8471d6555d5e9
Luis Saturno <saturno.luis@gmail.com>
2020-10-29 15:24:00 -0300

    Add items response formatters

--- api/api.js
+++ api/api.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const { searchResponseFormatter } = require('./formatters');
 
-function startAPI() {
+function main() {
   const app = express();
   const port = 4000;
   
   app.use(cors());
   
   app.get('/api', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello! This is the API');
   });
   
   app.get('/api/items', (req, res) => {
@@ -17,11 +18,12 @@ function startAPI() {
     const searchQuery = req.query["q"];
     const fetchURL = apiURL + new URLSearchParams({ q: searchQuery });
     
-    fetch(fetchURL).then(res => res.json()).then(data => console.log(JSON.stringify(data)));
-  
-    setTimeout(() => {
-      res.send([]);
-    }, 2000);
+    fetch(fetchURL)
+      .then(fetchResponse => fetchResponse.json())
+      .then(data => {
+        const formattedData = searchResponseFormatter(data);
+        res.send(formattedData);
+      });
   });
   
   app.listen(port, () => {
@@ -29,4 +31,4 @@ function startAPI() {
   });
 }
 
-startAPI();
+main();
---
+++ api/formatters.js
@@ -0,0 +1,38 @@
+const AUTHOR_NAME = "Luis";
+const AUTHOR_LASTNAME = "Saturno";
+const ITEMS_RESULTS_LIMIT = 4;
+
+function resultToItem(result) {
+  return {
+    id: result.id,
+    title: result.title,
+    price: {
+      currency: result.currency_id,
+      amount: result.price,
+      decimals: 0
+    },
+    picture: result.thumbnail,
+    condition: result.condition,
+    free_shipping: result.shipping.free_shipping
+  }
+}
+
+function getCategoriesFromFilters(filters) {
+  const categoryFilter = filters.find(filter => filter.id === 'category');
+  return categoryFilter.values[0].path_from_root;
+}
+
+function searchResponseFormatter(data) {
+  const formatted = {
+    author: {
+      name: AUTHOR_NAME,
+      lastname: AUTHOR_LASTNAME
+    },
+    categories: getCategoriesFromFilters(data.filters),
+    items: data.results.slice(0, ITEMS_RESULTS_LIMIT).map(resultToItem)
+  };
+
+  return [formatted];
+}
+
+module.exports = { searchResponseFormatter };
