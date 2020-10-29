d348a18c70c3a32cb35e16a29c699bf156d71146
Luis Saturno <saturno.luis@gmail.com>
2020-10-29 17:53:56 -0300

    Improve Breadcrumb implementation

--- front/src/components/Breadcrumb/Breadcrumb.js
+++ front/src/components/Breadcrumb/Breadcrumb.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 import './Breadcrumb.css';
 
 export default function Breadcrumb({ trail }) {
-  const trailLinks = trail.map(link => (<Link to="/">{link.name}</Link>));
-
   return(
     <nav className="Breadcrumb">
-      {trailLinks}
+      {trail.map(link => (
+        <Link to='/'>{link.name}</Link>
+      ))}
     </nav>
   );
 }
