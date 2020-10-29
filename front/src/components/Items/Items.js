1757dbabd2e7657dc1eb1fa482745cdb8f5eb27d
Luis Saturno <saturno.luis@gmail.com>
2020-10-29 18:31:44 -0300

    Add initial Items html markup

--- front/src/components/Items/Items.js
+++ front/src/components/Items/Items.js
@@ -1,9 +1,37 @@
+import { Link } from 'react-router-dom';
 import './Items.css';
 
+function ItemPrice({ data }) {
+  return (
+    <p>{JSON.stringify(data)}</p>
+  );
+}
+
+function ItemCondition({ condition }) {
+  return (
+    <p>{condition}</p>
+  );
+}
+
+
+function renderItem(item) {
+  const itemURL = `/items/${item.id}`;
+  return (
+    <article>
+      <Link to={itemURL}>
+        <img src={item.picture} alt={item.title} />
+      </Link>
+      <Link to={itemURL}>{item.title}</Link>
+      <ItemPrice data={item.price} />
+      <ItemCondition condition={item.condition} />
+    </article>
+  );
+}
+
 export default function Items({ items }) {
-    return (
-        <ul className="Items">
-            <li>{JSON.stringify(items)}</li>
-        </ul>
-    );
+  return (
+    <div className="Items">
+      {items.map(renderItem)}
+    </div>
+  );
 }
