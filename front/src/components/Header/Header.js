0e3bcfe88b8b821f76ef0ab2e01fcc6e70886240
Luis Saturno <saturno.luis@gmail.com>
2020-11-04 19:04:02 -0300

    Add missing alt attribute

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="Header">
       <Link className="Header__logo" to="/">
-        <img src="/logo.webp" />
+        <img src="/logo.webp" alt="Mercadolibre" />
       </Link>
       <SearchBox />
     </header>
